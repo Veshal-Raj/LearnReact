ee138412a8171503f9c6e74322e0460e1d377d04
Veshal Raj <veshalraj1307@gmail.com>
2023-10-30 21:56:11 +0530

    nested div or elements

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
-const heading = React.createElement(
-    "h1",
-    { id: 'Heading'}, 
-    "Hello World from React!"
-    )
+/**
+ * 
+ *  <div id='parent'> 
+ *      <div id="child">
+ *          <h1>I'm a h1 tag</h1>
+ *      </div>
+ *  </div>
+ * 
+ * 
+ * 
 
-    console.log(heading) // This will give you an object, this is a react element. React element is nothing, it is just js.
-    // And it have props also, props are childern + attribute, for example : the "Hello world from REact" and id: "Heading"
-        
+ */
+
+const parent = React.createElement(
+    "div",
+    {id: "parent"},
+    React.createElement(
+        "div",
+        {id: "child"},
+        React.createElement(
+            "h1",
+            {},
+            "I'm a h1 tag"
+            )
+    )
+)
+
+
+
+
+    console.log(parent) // object
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
    
-root.render(heading);
-// What this render is doing that, it pass the heading, which is an object and create the html tag that the browser understand.
+root.render(parent);
 
