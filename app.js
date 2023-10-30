d965fade9ff8ea105a7d3fbe63196e51fbaff668
Veshal Raj <veshalraj1307@gmail.com>
2023-10-30 22:11:50 +0530

    2 nested div

--- app.js
+++ app.js
@@ -5,6 +5,10 @@
  *          <h1>I'm a h1 tag</h1>
  *          <h2>I'm a h2 tag</h2>
  *      </div>
+*       <div id="child2">
+*           <h1>I'm a h1 tag</h1>
+*           <h2>I'm a h2 tag</h2>
+*          </div>
  *  </div>
  * 
  * 
@@ -16,10 +20,16 @@
 const parent = React.createElement(
     "div",
     {id: "parent"},
-    React.createElement("div",{id: "child"},[
-        React.createElement("h1", {} ,"I'm a h1 tag"),
-        React.createElement("h2", {} ,"I'm a h2 tag"),
-    ])
+    [
+        React.createElement("div",{id: "child"},[
+            React.createElement("h1", {} ,"I'm a h1 tag"),
+            React.createElement("h2", {} ,"I'm a h2 tag"),
+        ]),
+        React.createElement("div",{id: "child2"},[
+            React.createElement("h1", {} ,"I'm a h1 tag"),
+            React.createElement("h2", {} ,"I'm a h2 tag"),
+        ])
+    ]
 )
 
 
