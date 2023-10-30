90f2de79a0c7db070f7000231ae4b5b823b98917
Veshal Raj <veshalraj1307@gmail.com>
2023-10-30 21:43:06 +0530

    createElement and render()

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const heading = React.createElement(
     "h1",
-    { id: 'Heading'}, // these object are for giving attribute, for example in this case, for the h1 tag, the id Heading is given 
+    { id: 'Heading'}, 
     "Hello World from React!"
     )
+
+    console.log(heading) // This will give you an object, this is a react element. React element is nothing, it is just js.
+    // And it have props also, props are childern + attribute, for example : the "Hello world from REact" and id: "Heading"
         
 const root = ReactDOM.createRoot(document.getElementById("root"));
    
 root.render(heading);
+// What this render is doing that, it pass the heading, which is an object and create the html tag that the browser understand.
 
