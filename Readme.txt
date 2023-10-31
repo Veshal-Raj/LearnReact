a3cac52d6cd52d277098955a1fb07e611cc9c655
Veshal Raj <veshalraj1307@gmail.com>
2023-11-01 00:39:26 +0530

    igniting out app

--- Readme.txt
+++ Readme.txt
@@ -1,2 +1,21 @@
 Order of the files matter alot
 root also matter, where we are rendering
+
+# Parcel
+- Dev build
+- Local server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optmization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- Https
+- Tree Shaking - remove unused code
+- Different dev and production bundles
--- app.js
+++ app.js
@@ -1,29 +1,13 @@
-/**
- * 
- *  <div id='parent'> 
- *      <div id="child">
- *          <h1>I'm a h1 tag</h1>
- *          <h2>I'm a h2 tag</h2>
- *      </div>
-*       <div id="child2">
-*           <h1>I'm a h1 tag</h1>
-*           <h2>I'm a h2 tag</h2>
-*          </div>
- *  </div>
- * 
+import React from "react";
+import ReactDOM from "react-dom/client";
 
- */
-
-
-//          This is the core of react. the below code is just an example for that, 
-//          react can be written without JSX
 
 const parent = React.createElement(
     "div",
     {id: "parent"},
     [
         React.createElement("div",{id: "child"},[
-            React.createElement("h1", {} ,"I'm a h1 tag"),
+            React.createElement("h1", {} ,"This is Veshal"),
             React.createElement("h2", {} ,"I'm a h2 tag"),
         ]),
         React.createElement("div",{id: "child2"},[
@@ -32,11 +16,8 @@ const parent = React.createElement(
         ])
     ]
 )
-
-
-
-
-    console.log(parent) // object
+ 
+console.log(parent) // object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
    
